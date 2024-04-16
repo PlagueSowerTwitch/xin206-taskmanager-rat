092354c5d45ec01f37aab5c959ad5c01f5ca37a1
Yann <yannthon2004@gmail.com>
2024-04-16 15:41:24 +0200

    Modification du menu

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fonctionnalite import *
 
-def afficher_menu():
+def afficher_menu(taches):
+    afficher_taches(taches)
     print("\nMenu:")
     print("1. Ajouter une tâche")
     print("2. Supprimer une tâche")
     print("3. Marquer une tâche comme terminée")
-    print("4. Afficher les tâches")
-    print("5. Quitter")
+    print("4. Quitter")
 
 if __name__ == "__main__":
     taches = charger_taches()
